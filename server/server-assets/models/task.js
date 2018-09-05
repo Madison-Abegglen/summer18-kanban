e44a1a2cd77f433d4df7414bf47f1b3deb469b3b
Madison Abegglen <madison.abegglen@gmail.com>
2018-09-05 09:39:10 -0600

    schema

--- server/server-assets/models/task.js
+++ server/server-assets/models/task.js
@@ -1,8 +1,34 @@
 const mongoose = require('mongoose')
 const { Schema } = mongoose
 
+const comment = new Schema({
+  content: {
+    type: String,
+    required: true
+  },
+  created: {
+    type: Number,
+    required: true,
+    default: Date.now()
+  }
+})
+
 const schema = new Schema({
-  
+  content: {
+    type: String,
+    required: true
+  },
+  created: {
+    type: Number,
+    required: true,
+    default: Date.now()
+  },
+  listId: {
+    type: Schema.Types.ObjectId,
+    ref: 'List',
+    required: true
+  },
+  comments: [comment]
 })
 
 module.exports = mongoose.model('Task', schema)
