a3c96a3be1d2e317b9b72521dea990202d55e1be
Madison Abegglen <madison.abegglen@gmail.com>
2018-09-06 10:09:50 -0600

    style style ye ye

--- client/src/components/List.vue
+++ client/src/components/List.vue
@@ -1,7 +1,11 @@
 <template>
-  <mdc-card>
-    <mdc-card-header :title="$props.listData.title">
-    </mdc-card-header>
+  <mdc-card class="list-card">
+    <div class="list-header">
+      <mdc-card-header :title="$props.listData.title">
+      </mdc-card-header>
+      <mdc-icon icon="add"></mdc-icon>
+    </div>
+
   </mdc-card>
 </template>
 
@@ -12,5 +16,19 @@ export default {
 };
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+.list-header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
 </style>
+<style lang="scss">
+.list-card {
+  width: 60rem;
+  max-width: 100%;
+  margin: 1rem auto;
+  padding: 0 1.5rem;
+}
+</style>
+
