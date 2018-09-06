bb17d4bf4ad7abc343cb6dd6d5d9073834d99289
Christopher Brown <chbphone55@gmail.com>
2018-09-06 09:33:10 -0600

    puuusssh it

--- client/src/App.vue
+++ client/src/App.vue
@@ -7,12 +7,17 @@
 
 <style>
 :root {
-  --mdc-theme-primary: unset !important;
-  --mdc-theme-secondary: unset !important;
+  /* --mdc-theme-primary: #5da0a2 !important;
+  --mdc-theme-secondary: #f4f7f7 !important;
+  --mdc-theme-background: #aacfd0 !important; */
+  --mdc-theme-primary: #f8b500 !important;
+  --mdc-theme-secondary: #393e46 !important;
+  --mdc-theme-background: #f7f7f7 !important;
 }
 body {
   height: 100vh;
   margin: 0;
+  background-color: var(--mdc-theme-background);
 }
 #app {
   -webkit-font-smoothing: antialiased;
