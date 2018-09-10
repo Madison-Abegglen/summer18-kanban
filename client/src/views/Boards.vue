78ce66d748cf2528f6966e1cc3358bae5c715b36
Madison Abegglen <madison.abegglen@gmail.com>
2018-09-10 10:58:42 -0600

    woo descriptions for boards

--- client/src/views/Boards.vue
+++ client/src/views/Boards.vue
@@ -2,7 +2,9 @@
   <main class='boards'>
     <mdc-top-app-bar icon='exit_to_app' dense title='WELCOME TO YOUR BOARDS' v-on:nav='$store.dispatch("logout")'>
     </mdc-top-app-bar>
+
     <mdc-fab fixed icon='add' @click='open = true'></mdc-fab>
+
     <mdc-dialog v-model='open' title='New Board' accept=''>
       <form @submit.prevent='addBoard' class="form">
         <mdc-textfield label='Title' v-model='newBoard.title' required />
@@ -10,61 +12,65 @@
         <mdc-button type='submit' @click='open = false' outlined>Create Board</mdc-button>
       </form>
     </mdc-dialog>
+
     <mdc-list bordered class='list'>
       <mdc-list-item class='list-item mdc-elevation mdc-elevation--z2' v-for='board in boards' :key='board._id'>
         <mdc-button class='board-title' :to='{name: "board", params: {boardId: board._id}}'>{{board.title}}</mdc-button>
+        <mdc-text class='board-des' typo='body2' tag='span'>{{board.description}}</mdc-text>
+
         <mdc-menu-anchor>
           <mdc-button @click='board.open = true' class='settings'>
             <i class='material-icons mdc-button__icon'>more_horiz</i>
           </mdc-button>
+         
           <mdc-menu v-model='board.open'>
             <mdc-button @click='deleteBoard(board._id)'>Remove</mdc-button>
-            <mdc-button>Rename</mdc-button>
             <mdc-button :to='{name: "board", params: {boardId: board._id}}'>Open</mdc-button>
           </mdc-menu>
         </mdc-menu-anchor>
       </mdc-list-item>
     </mdc-list>
+  
   </main>
 </template>
 
 <script>
 export default {
-  name: 'boards',
-  created () {
+  name: "boards",
+  created() {
     // blocks users not logged in
     if (!this.$store.state.user._id) {
-      this.$router.push({ name: 'login' })
+      this.$router.push({ name: "login" });
     }
   },
-  mounted () {
-    this.$store.dispatch('clearLists')
-    this.$store.dispatch('getBoards')
+  mounted() {
+    this.$store.dispatch("clearLists");
+    this.$store.dispatch("getBoards");
   },
-  data () {
+  data() {
     return {
       newBoard: {
-        title: '',
-        description: ''
+        title: "",
+        description: ""
       },
       open: false
-    }
+    };
   },
   computed: {
-    boards () {
-      return this.$store.state.boards
+    boards() {
+      return this.$store.state.boards;
     }
   },
   methods: {
-    addBoard () {
-      this.$store.dispatch('addBoard', this.newBoard)
-      this.newBoard = { title: '', description: '' }
+    addBoard() {
+      this.$store.dispatch("addBoard", this.newBoard);
+      this.newBoard = { title: "", description: "" };
     },
-    deleteBoard (boardId) {
-      this.$store.dispatch('deleteBoard', boardId)
+    deleteBoard(boardId) {
+      this.$store.dispatch("deleteBoard", boardId);
     }
   }
-}
+};
 </script>
 
 <style lang='scss' scoped>
@@ -118,6 +124,19 @@ header {
   &::after {
     background-color: transparent !important;
   }
+  white-space: nowrap;
+  flex-shrink: 0;
+}
+
+.board-des {
+  margin-right: auto;
+  border-left: solid 0.1rem var(--mdc-theme-primary);
+  margin-left: 0.25rem;
+  padding-left: 0.5rem;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  opacity: 0.75;
 }
 </style>
 <style lang='scss'>
