2a064c23254ce1f370adc24fba336297ae2b6f6e
Christopher Brown <chbphone55@gmail.com>
2018-09-13 10:19:11 -0600

    env

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const server = express()
 const app = require('http').createServer(server)
 const cors = require('cors')
-const port = process.event.PORT || 3000
+const port = process.env.PORT || 3000
 
 app.use(express.static(__dirname + '/../client/dist/'))
 
